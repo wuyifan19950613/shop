15a379b24f91b4900dd52c1ad2823c10b8e96bf7
wuyifan1995 <502397665@qq.com>
2018-11-05 11:16:26 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,26 @@ import {myfun} from '../test/js/Wuyifan-font.js' //注意路径
 export default {
   name: 'App',
   mounted() {
+    const vds = document.createElement('script')
+    vds.type = 'text/javascript'
+    vds.async = true
+    vds.src = (document.location.protocol === 'https:' ? 'https://' : 'http://') +
+      's5.cnzz.com/z_stat.php?id=1275250107&web_id=1275250107'
+    const s = document.getElementsByTagName('script')[0]
+    s.parentNode.insertBefore(vds, s)
+  },
+  watch: {
+    '$route' () {
+      if (window._czc) {
+        let location = window.location
+        let contentUrl = location.pathname + location.hash
+        let refererUrl = '/'
+        window._czc.push(['_trackPageview', contentUrl, refererUrl])
+      }
+    }
   },
   methods: {
-    methodName() {
 
-    }
   },
 }
 </script>
