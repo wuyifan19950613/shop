9a48ae8921dd4ec3df11c27f99e0a9fad4941e99
wuyifan1995 <502397665@qq.com>
2018-11-19 17:30:30 +0800

    优化

--- src/utils/keyWords.js
+++ src/utils/keyWords.js
@@ -3,7 +3,120 @@ const keyWords = {
     title: '精选淘宝天猫优惠卷-小欢有劵',
     meta: [{
       name: 'keyWords',
-      content: '卫衣,大码卫衣,女装卫衣,卫衣女,卫衣女套装,卫衣品牌,卫衣套装,长裤卫衣,情侣卫衣,韩版卫衣,带帽卫衣,情侣装,加厚卫衣,卫衣 女,运动套装,小熊卫衣,棉衣,优衣库,weiyi,'
+      content: '卫衣,大码卫衣,女装卫衣,卫衣女,卫衣女套装,卫衣品牌,卫衣套装,长裤卫衣,情侣卫衣,韩版卫衣,带帽卫衣,情侣装,加厚卫衣,卫衣 女,运动套装,小熊卫衣,棉衣,优衣库,weiyi'
+    },{
+      name: 'description',
+      content: '',
+    }]
+  },
+  jiakeMate: {
+    title: '精选淘宝天猫优惠卷-小欢有劵',
+    meta: [{
+      name: 'keyWords',
+      content: ''
+    },{
+      name: 'description',
+      content: '',
+    }]
+  },
+  chenshanMate: {
+    title: '精选淘宝天猫优惠卷-小欢有劵',
+    meta: [{
+      name: 'keyWords',
+      content: ''
+    },{
+      name: 'description',
+      content: '',
+    }]
+  },
+  banxieMate: {
+    title: '精选淘宝天猫优惠卷-小欢有劵',
+    meta: [{
+      name: 'keyWords',
+      content: ''
+    },{
+      name: 'description',
+      content: '',
+    }]
+  },
+  xiuxianMate: {
+    title: '精选淘宝天猫优惠卷-小欢有劵',
+    meta: [{
+      name: 'keyWords',
+      content: ''
+    },{
+      name: 'description',
+      content: '',
+    }]
+  },
+  xueziMate: {
+    title: '精选淘宝天猫优惠卷-小欢有劵',
+    meta: [{
+      name: 'keyWords',
+      content: ''
+    },{
+      name: 'description',
+      content: '',
+    }]
+  },
+  niuzaikuMate: {
+    title: '精选淘宝天猫优惠卷-小欢有劵',
+    meta: [{
+      name: 'keyWords',
+      content: ''
+    },{
+      name: 'description',
+      content: '',
+    }]
+  },
+  xiuxiankuMate: {
+    title: '精选淘宝天猫优惠卷-小欢有劵',
+    meta: [{
+      name: 'keyWords',
+      content: ''
+    },{
+      name: 'description',
+      content: '',
+    }]
+  },
+  shuangjianbaoMate: {
+    title: '精选淘宝天猫优惠卷-小欢有劵',
+    meta: [{
+      name: 'keyWords',
+      content: ''
+    },{
+      name: 'description',
+      content: '',
+    }]
+  },
+  maoziMate: {
+    title: '精选淘宝天猫优惠卷-小欢有劵',
+    meta: [{
+      name: 'keyWords',
+      content: ''
+    },{
+      name: 'description',
+      content: '',
+    }]
+  },
+  shoushiMate: {
+    title: '精选淘宝天猫优惠卷-小欢有劵',
+    meta: [{
+      name: 'keyWords',
+      content: ''
+    },{
+      name: 'description',
+      content: '',
+    }]
+  },
+  waziMate: {
+    title: '精选淘宝天猫优惠卷-小欢有劵',
+    meta: [{
+      name: 'keyWords',
+      content: ''
+    },{
+      name: 'description',
+      content: '',
     }]
   },
 }
